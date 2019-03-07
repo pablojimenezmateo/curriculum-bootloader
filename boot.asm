63e3bcc0bd00cf58c9b05e5212fc702deca7405c
Pablo Jimenez Mateo <gef3233@gmail.com>
2019-03-07 12:58:34 -0800

    Refactored border code

--- boot.asm
+++ boot.asm
@@ -38,109 +38,81 @@ start:
 
 draw_border:
 
-    ; top border
-    ; x from 0 to 304 in increments of 16 (19 times)
+xor cx, cx ; draw horizontally
+xor dx, dx ; border index
 
-    xor si, si ; si = 0
+.top_border_init:
+    xor si, si
+    xor di, di
+    mov ax, 320
+    jmp .draw
 
-.top_border:
+.bottom_left_border_init:
 
-    push 0                  ; initial y position to draw the rock
-    push si                 ; initial x position to draw the rock
-    call draw_rock_tile
+    mov si, 176
+    xor di, di
+    mov ax, 128
+    jmp .draw
 
-    add si, 16
+.bottom_right_border_init:
 
-    cmp si, 320
-    je .right_border_init
+    mov di, 192
+    mov ax, 304
+    jmp .draw
 
-    jmp .top_border
-
-;------------------------ Vertical borders
 .right_border_init:
 
-    mov si, 16
-    mov di, 0
-    jmp .vertical_border
+    xor si, si
+    xor di, di
+    mov ax, 192
+    mov cx, 1 ; draw vertically
+    jmp .draw
 
 .left_border_init:
 
-    mov si, 16
+    xor si, si
     mov di, 304
 
-.vertical_border:
+.draw:
 
     push si                  ; initial y position to draw the rock
     push di                   ; initial x position to draw the rock
     call draw_rock_tile  
 
-    add si, 16
+    cmp cx, 0  ;if horizontal
+    jne .vertical_index_update
 
-    cmp si, 192
+    add di, 16
+    cmp di, ax
     je .check_finish
 
-    jmp .vertical_border
+    jmp .draw
+
+.vertical_index_update:
+    add si, 16
+    cmp si, ax
+    je .check_finish
+
+    jmp .draw
 
 
 .check_finish:
 
-    cmp di, 304
-    je .bottom_border_left_init
-    jmp .left_border_init
+    inc dx       ; we have finished a border
 
-;.right_border_init:
-;
-;    mov si, 16
-;
-;.right_border:
-;
-;    push si                  ; initial y position to draw the rock
-;    push di                 ; initial x position to draw the rock
-;    call draw_rock_tile  
-;
-;    add si, 16
-;
-;    cmp si, 192
-;    je .bottom_border_left_init
-;
-;    jmp .right_border
+    cmp dx, 1
+    je .bottom_left_border_init
 
+    cmp dx, 2
+    je .bottom_right_border_init
 
-;---------------------- Vertical borders
+    cmp dx, 3
+    je .right_border_init
+   
+    cmp dx, 4
+    je .left_border_init
 
-.bottom_border_left_init:
-
-    mov si, 16
-
-.bottom_border_left:
-
-    push 176                  ; initial y position to draw the rock
-    push si                 ; initial x position to draw the rock
-    call draw_rock_tile
-
-    add si, 16
-
-    cmp si, 128
-    je .bottom_border_right_init
-
-    jmp .bottom_border_left
-
-.bottom_border_right_init:
-
-    mov si, 192
-
-.bottom_border_right:
-
-    push 176                  ; initial y position to draw the rock
-    push si                 ; initial x position to draw the rock
-    call draw_rock_tile
-
-    add si, 16
-
-    cmp si, 304
-    je .done
-
-    jmp .bottom_border_right
+    jmp .done
 
 .done:
     ret
