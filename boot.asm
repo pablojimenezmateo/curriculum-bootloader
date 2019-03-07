3548f5dc21c7356c8194f802451dc5ee1e097af6
Pablo Jimenez Mateo <gef3233@gmail.com>
2019-03-07 12:00:59 -0800

    Added sprites, started shortening code

--- boot.asm
+++ boot.asm
@@ -17,10 +17,17 @@ start:
 
     int 10h                 ; call
 
+    push 9
+    push 5
+    push 20
+    push msg1
+    call print_text
 
-    call print_text_1
-
-    call print_text_2
+    push 11
+    push 6
+    push 17
+    push msg2
+    call print_text
 
     call draw_border
 
@@ -34,7 +41,7 @@ draw_border:
     ; top border
     ; x from 0 to 304 in increments of 16 (19 times)
 
-    mov si, 0
+    xor si, si ; si = 0
 
 .top_border:
 
@@ -127,14 +134,15 @@ draw_rock_tile:
     pusha
 
     mov ah, 0Ch
-    mov bh, 0              ; page number
+    xor bh, bh             ; page number 0
 
-    mov cx, [bp + 4]       ; first argument
-    mov dx, [bp + 6]       ; second argument
+    mov cx, [bp + 4]       ; x coordinate
+    mov dx, [bp + 6]       ; y coordinate
     push cx                ; we need to store the x value for .next_row
-
-    mov si, 0              ; index of the bit we are checking (width)
-    mov di, 0              ; index of the bit we are checking (height)
+     
+                            ; initializing to 0, saves one byte from using mov
+    xor si, si              ; index of the bit we are checking (width)
+    xor di, di              ; index of the bit we are checking (height)
 
 .row: ; main loop, this will iterate over every bit of [rock], if it is a 1 the .one part will be executed, if it is a 0 the .zero part will
     
@@ -146,25 +154,22 @@ draw_rock_tile:
 
     push dx
     mov dx, [rock + di]    ; load the bitpattern
-    bt dx, si      ; check the si th bit and store it on cf
+    bt dx, si              ; check the si th bit and store it on cf
     pop dx
 
     jc .one
-    jmp .zero
+
+.zero:
+
+    xor al, al       ; color black
+    jmp .draw
 
 .one:
 
     mov al, 06h       ; color brown
-    int 10h
 
-    inc si
-    inc cx
+.draw:
 
-    jmp .row
-
-.zero:
-
-    mov al, 00h       ; color black
     int 10h
 
     inc si
@@ -175,7 +180,7 @@ draw_rock_tile:
 .next_row:
 
     add di, 2  ; next byte
-    mov si, 0  ; firs bit
+    xor si, si  ; firs bit
     inc dx     ; next row
 
     pop cx
@@ -191,39 +196,38 @@ draw_rock_tile:
     pop bp
     ret 4
 
-print_text_1:
+print_text:
+
+    push bp                ; save old base pointer
+    mov bp, sp             ; use the current stack pointer as new base pointer
+    pusha
 
     mov ax, 7c0h        ; beginning of the code
     mov es, ax
-    mov bp, msg1
-    mov ah,13h          ; function 13 - write string
-    mov al,01h          ; attrib in bl, move cursor
-    mov bl,0Fh          ; color white
-    mov bh, 0
-    mov cx,20           ; length of string
-    mov dh,5            ; row to put string
-    mov dl,9            ; column to put string
-    int 10h             ; call BIOS service
-    ret
+    mov cx, [bp + 6]            ; length of string
+    mov dh, [bp + 8]            ; row to put string
+    mov dl, [bp + 10]           ; column to put string
+    mov bp, [bp + 4]
 
-print_text_2:
+    mov ah, 13h          ; function 13 - write string
+    mov al, 01h          ; attrib in bl, move cursor
+    mov bl, 0Fh          ; color white
 
-    mov ax, 7c0h        ; beginning of the code
-    mov es, ax
-    mov bp, msg2
-    mov ah,13h          ; function 13 - write string
-    mov al,01h          ; attrib in bl, move cursor
-    mov bl,0Fh          ; color white
-    mov bh, 0
-    mov cx,17           ; length of string
-    mov dh,6            ; row to put string
-    mov dl,11            ; column to put string
     int 10h             ; call BIOS service
-    ret
+
+    popa
+    mov sp, bp
+    pop bp
+
+    ret 8
 
 msg1: db "IT'S DANGEROUS TO GO"
 msg2: db "ALONE!   HIRE ME."
 rock: dw 0xC3B7, 0xDFCF, 0xFFCF, 0x7FCF, 0x7FE6, 0xFFEF, 0xBFEF, 0xBFEF, 0x7FE7, 0xFFEF, 0x7DE7, 0x3C9B, 0x7DFD, 0xBC7D, 0xFCFF, 0x2CFC ; 32 bytes
+fire: dw 0x2020, 0x8020, 0x8800, 0xA810, 0xA880, 0xA288, 0xA6A8, 0xAAA2, 0x9AA2, 0x66AA, 0x55AA, 0x7568, 0xFD68, 0xF5A0, 0x56A0, 0xAA00 ; 32 bytes
+wiseman: dw 0x5400, 0x7700, 0x4500, 0x4500, 0x5E00, 0xFF80, 0x0FA0, 0xFBE8, 0xFAE9, 0xFAA9, 0xE8A9, 0xA8A8, 0xA8A8, 0xAA20, 0xAA00, 0x9680; 32 bytes
 
-times 510 - ($ - $$) db 0   ; padding with 0 at the end
+gef:     dw 0xAA00, 0xAA80, 0xAAA0, 0x88A8, 0x1818, 0x5158, 0x5558, 0x56A8, 0x5558, 0x5018, 0x5ED8, 0x5ED0, 0x9550, 0x65A0, 0x2A80, 0xBC00, 0x3F00, 0x33C0, 0x30F0, 0x3040, 0x0000, 0x2000; 44 bytes
+
+;times 510 - ($ - $$) db 0   ; padding with 0 at the end
 dw 0xAA55                   ; PC boot signature
