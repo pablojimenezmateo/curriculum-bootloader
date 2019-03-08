94cc99f49eb899c3e8b3e8070d3192aca847ec26
Pablo Jimenez Mateo <gef3233@gmail.com>
2019-03-07 23:44:57 -0800

    Finished?

--- boot.asm
+++ boot.asm
@@ -257,31 +257,107 @@ rock:    dw 0xC3B7, 0xDFCF, 0xFFCF, 0x7FCF, 0x7FE6, 0xFFEF, 0xBFEF, 0xBFEF, 0x7F
 times 510 - ($ - $$) db 0   ; padding with 0 at the end
 dw 0xAA55                   ; PC boot signature
 
-;times  566 - ($ - $$) db 0   ; padding with 0 at the end
 
 stage2:
 
-    push 90                   ; y
-    push 144                   ; x
-    push 06h                  ; first color, brown
-    push 0Ch                  ; second color, red
-    push word [wiseman_left]  ; sprite to draw
+    ; copy the current sprite
+    mov      cx, 32
+    lea      di, [current_sprite]
+    lea      si, [wiseman_left]
+    rep      movsb
+
     push 32                   ; how many bytes the sprite has
+    push 06h                  ; first color, brown
+    push 04h                  ; second color, red
+    push 90                   ; y
+    push 152                  ; x
+    call draw_sprite
+
+    ; copy the current sprite
+    mov      cx, 32
+    lea      di, [current_sprite]
+    lea      si, [wiseman_right]
+    rep      movsb
+
+    ;push 90                    ; y
+    push 160                   ; x
+    call draw_sprite
+
+    ; copy the current sprite
+    mov      cx, 32
+    lea      di, [current_sprite]
+    lea      si, [fire_left]
+    rep      movsb
+
+    pop si
+    pop si
+    pop si
+    push 0Eh                  ; first color, yellow
+    push 0Ch                  ; second color, red
+    push 90                   ; y
+    push 80                   ; x
+    call draw_sprite
+
+    ; second fire left
+    ;push 90                    ; y
+    push 224                   ; x
+    call draw_sprite
+
+
+    ; copy the current sprite
+    mov      cx, 32
+    lea      di, [current_sprite]
+    lea      si, [fire_right]
+    rep      movsb
+
+    ;push 90                   ; y
+    push 88                   ; x
+    call draw_sprite
+
+    ;push 90                    ; y
+    push 232                   ; x
+    call draw_sprite
+
+    ; copy the current sprite
+    mov      cx, 44
+    lea      di, [current_sprite]
+    lea      si, [gef_left]
+    rep      movsb
+
+    pop si
+    pop si
+    pop si
+    pop si
+
+    push 44                   ; how many bytes the sprite has
+    push 07h                  ; first color, brown
+    push 06h                  ; second color, red
+    push 120                   ; y
+    push 152                   ; x
+    call draw_sprite
+
+    ; copy the current sprite
+    mov      cx, 44
+    lea      di, [current_sprite]
+    lea      si, [gef_right]
+    rep      movsb
+
+    ;push 120                   ; y
+    push 160                   ; x
     call draw_sprite
 
    cli
    hlt
-
 
 ; 00 is always black and 11 is always white
 draw_sprite:
 
     push bp                ; save old base pointer
     mov bp, sp             ; use the current stack pointer as new base pointer
-    pusha
+    ;pusha
 
-    mov cx, [bp + 12]       ; x coordinate
-    mov dx, [bp + 14]       ; y coordinate
+    mov cx, [bp + 4]       ; x coordinate
+    mov dx, [bp + 6]       ; y coordinate
     
                             ; initializing to 0, saves one byte from using mov
     xor si, si              ; index of the bit we are checking (width)
@@ -294,17 +370,17 @@ draw_sprite:
 
     xor si, si           ; this executes if we move to the next row
     add di, 2            ; next row
-    cmp di, [bp + 4]     ; if we have finished with the tile
+    cmp di, [bp + 12]     ; if we have finished with the tile
     je .done
     inc dx
 
-    mov cx, [bp + 12]       ; x coordinate
+    mov cx, [bp + 4]       ; x coordinate
 
 .same_row:
 
     xor bh, bh              ; store the color
 
-    mov ax, [wiseman_left + di]
+    mov ax, [current_sprite + di]
 
     bt ax, si              ; first bit
     jnc .next_bit
@@ -320,20 +396,19 @@ draw_sprite:
     cmp bh, 0              ; black
     je .pass
 
+    mov ah, 0Ch ; draw
+
     cmp bh, 1              ; first_color
     je .first_color
 
     cmp bh, 2              ; second_color
     je .second_color
 
-    cmp bh, 3              ; white
-    je .white
+    jmp .white
 
 .first_color:
 
     ; draw
-    mov ah, 0Ch
-    xor bh, bh
     mov al, [bp + 10]
     int 10h
     jmp .pass
@@ -341,8 +416,6 @@ draw_sprite:
 .second_color:
 
     ; draw
-    mov ah, 0Ch
-    xor bh, bh        
     mov al, [bp + 8]
     int 10h
     jmp .pass
@@ -350,8 +423,6 @@ draw_sprite:
 .white:
 
     ; draw
-    mov ah, 0Ch
-    xor bh, bh
     mov al, 0Fh
     int 10h
     jmp .pass
@@ -362,17 +433,19 @@ draw_sprite:
     jmp .row
 
 .done:
-    popa
+    ;popa
     mov sp, bp
     pop bp
-    ret 12
+    ret 2 ; I only pop the y
     
 
-fire_left:    dw 0x2020, 0x8020, 0x8800, 0xA810, 0xA880, 0xA288, 0xA6A8, 0xAAA2, 0x9AA2, 0x66AA, 0x55AA, 0x7568, 0xFD68, 0xF5A0, 0x56A0, 0xAA00 ; 32 bytes
-fire_right:   dw 0x0808, 0x0802, 0x0022, 0x042A, 0x022A, 0x228A, 0x2A9A, 0x8AAA, 0x8AA6, 0xAA99, 0xAA55, 0x295D, 0x297F, 0x0A5F, 0x0A95, 0x00AA ; 32 bytes
-wiseman_left: dw 0x5400, 0x7700, 0x4500, 0x4500, 0x5E00, 0xFF80, 0x0FA0, 0xFBE8, 0xFAE9, 0xFAA9, 0xE8A9, 0xA8A8, 0xA8A8, 0xAA20, 0xAA00, 0x9680 ; 32 bytes
+current_sprite: dw 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000,0x0000, 0x0000, 0x0000, 0x0000 ; 32 bytes       
+
+fire_left:     dw 0x2020, 0x8020, 0x8800, 0xA810, 0xA880, 0xA288, 0xA6A8, 0xAAA2, 0x9AA2, 0x66AA, 0x55AA, 0x7568, 0xFD68, 0xF5A0, 0x56A0, 0xAA00 ; 32 bytes
+fire_right:    dw 0x0808, 0x0802, 0x0022, 0x042A, 0x022A, 0x228A, 0x2A9A, 0x8AAA, 0x8AA6, 0xAA99, 0xAA55, 0x295D, 0x297F, 0x0A5F, 0x0A95, 0x00AA ; 32 bytes
+wiseman_left:  dw 0x5400, 0x7700, 0x4500, 0x4500, 0x5E00, 0xFF80, 0x0FA0, 0xFBE8, 0xFAE9, 0xFAA9, 0xE8A9, 0xA8A8, 0xA8A8, 0xAA20, 0xAA00, 0x9680 ; 32 bytes
 wiseman_right: dw 0x0015, 0x00dd, 0x0051, 0x0051, 0x00b5, 0x02ff, 0x0af0, 0x2bef, 0x6baf, 0x6aaf, 0x6a2b, 0x2a2a, 0x2a2a, 0x08aa, 0x00aa, 0x0296 ; 32 bytes
-gef_left:     dw 0xAA00, 0xAA80, 0xAAA0, 0x88A8, 0x1818, 0x5158, 0x5558, 0x56A8, 0x5558, 0x5018, 0x5ED8, 0x5ED0, 0x9550, 0x65A0, 0x2A80, 0xBC00, 0x3F00, 0x33C0, 0x30F0, 0x3040, 0x0000, 0x2000 ; 44 bytes
-gef_right: dw 0x00aa, 0x02aa, 0x0aaa, 0x2a22, 0x2424, 0x2545, 0x2555, 0x2a95, 0x2555, 0x2405, 0x27b5, 0x07b5, 0x0556, 0x0a59, 0x02a8, 0x003e, 0x00fc, 0x03cc, 0x0f0c, 0x010c, 0x0000, 0x0008 ; 44 bytes
+gef_left:      dw 0xAA00, 0xAA80, 0xAAA0, 0x88A8, 0x1818, 0x5158, 0x5558, 0x56A8, 0x5558, 0x5018, 0x5ED8, 0x5ED0, 0x9550, 0x65A0, 0x2A80, 0xBC00, 0x3F00, 0x33C0, 0x30F0, 0x3040, 0x0000, 0x2000 ; 44 bytes
+gef_right:     dw 0x00aa, 0x02aa, 0x0aaa, 0x2a22, 0x2424, 0x2545, 0x2555, 0x2a95, 0x2555, 0x2405, 0x27b5, 0x07b5, 0x0556, 0x0a59, 0x02a8, 0x003e, 0x00fc, 0x03cc, 0x0f0c, 0x010c, 0x0000, 0x0008 ; 44 bytes
 
 times 1024 - ($-$$) db 0 ; 1024 maximum size
