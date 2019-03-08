d81018336b9478f9f38978442e107a098ed739db
Pablo Jimenez Mateo <gef3233@gmail.com>
2019-03-07 17:45:57 -0800

    Almost half wise

--- boot.asm
+++ boot.asm
@@ -53,13 +53,7 @@ start:
 
     call draw_border
 
-    ;push 50                   ; x
-    ;push 50                   ; y
-    ;push 0Ch                  ; first color, red
-    ;push 0Eh                  ; second color, yellow
-    ;push fire                 ; sprite to draw
-    ;push 16                   ; how many rows the sprite has
-    ;call draw_sprite
+    ;----- Not enough space for sprites, move that to the second stage
 
     ; Load stage 2 to memory.
     mov ah, 0x02
@@ -78,11 +72,6 @@ start:
     int 0x13
 
     jmp stage2
-
-
-
-    cli ; stop execution
-    hlt
 
 ; ----------------------------------------------------------------------
 
@@ -193,7 +182,6 @@ draw_rock_tile:
 
     mov cx, [bp + 4]       ; x coordinate
 
-
 .same_row:
 
     mov ax, [rock + di]
@@ -242,101 +230,6 @@ print_text:
 
     ret 8
 
-
-; 00 is always black and 11 is always white
-;draw_sprite:
-;
-;    push bp                ; save old base pointer
-;    mov bp, sp             ; use the current stack pointer as new base pointer
-;    pusha
-;
-;    mov ah, 0Ch
-;    xor bh, bh             ; page number 0
-;
-;    mov cx, [bp + 12]       ; x coordinate where to draw the sprite
-;    mov dx, [bp + 14]       ; y coordinate where to draw the sprite
-;    push cx                 ; we need to store the x value for .next_row
-;     
-;                            ; initializing to 0, saves one byte from using mov
-;    xor si, si              ; index of the bit we are checking (width)
-;    xor di, di              ; index of the bit we are checking (height)
-;
-;.row:
-;    
-;    push dx
-;    push ax
-;    mov ax, [bp + 4]       ; sprite address
-;
-;    ; most significant
-;    mov dx, [bp + 4 + di]      ; load byte  00 10 00 00 # 00 10 00 00
-;    bt dx, si              ; check the si th bit and store it on cf
-;
-;    xor ax, ax             ; store the color index
-;
-;    jnc .zero_1                ; if the bit is zero
-;    add ax, 2
-;    inc si
-;
-;.zero_1:
-;    ; less significant
-;    mov dx, [bp + 4 + di]      ; load byte  00 10 00 00 # 00 10 00 00
-;    bt dx, si              ; check the si th bit and store it on cf
-;    jnc .zero_2
-;    add ax, 1
-;    inc si
-;
-;.zero_2:
-;    cmp ax, 0              ; black
-;    je .black
-;
-;    cmp ax, 1              ; first color
-;    je .first_color
-;
-;    cmp ax, 2              ; second color
-;    je .second_color
-;
-;    cmp ax, 3              ; white
-;    je .white
-;
-;
-;.black:
-;    pop ax
-;    pop dx
-;
-;    mov al, 01h            ; Brown
-;    int 10h
-;    jmp .row
-;
-;
-;.first_color:
-;    pop ax
-;    pop dx
-;    mov al, 02h            ; Brown
-;    int 10h
-;    jmp .row
-;
-;.second_color:
-;    pop ax
-;    pop dx
-;    mov al, 03h            ; Brown
-;    int 10h
-;    jmp .row
-;
-;.white:
-;    pop ax
-;    pop dx
-;    mov al, 0fh            ; Brown
-;    int 10h
-;    jmp .row
-;
-;.done:
-;    popa
-;    mov sp, bp
-;    pop bp
-;
-;    ret 12
-
-msg0:    db "EAT THIS"
 msg1:    db "IT'S DANGEROUS TO GO"
 msg2:    db "ALONE!   HIRE ME."
 rock:    dw 0xC3B7, 0xDFCF, 0xFFCF, 0x7FCF, 0x7FE6, 0xFFEF, 0xBFEF, 0xBFEF, 0x7FE7, 0xFFEF, 0x7DE7, 0x3C9B, 0x7DFD, 0xBC7D, 0xFCFF, 0x2CFC ; 32 bytes
@@ -347,17 +240,120 @@ dw 0xAA55                   ; PC boot signature
 
 stage2:
 
-   push 11                 ; column
-   push 6                  ; row
-   push 17                 ; msg length
-   push msg2               ; msg to write
-   call print_text
+    push 50                   ; y
+    push 50                   ; x
+    push 0Ch                  ; first color, red
+    push 0Eh                  ; second color, yellow
+    push fire                 ; sprite to draw
+    push 16                   ; how many rows the sprite has
+    call draw_sprite
+
+    push 11                 ; column
+    push 6                  ; row
+    push 17                 ; msg length
+    push msg2               ; msg to write
+    call print_text
 
    cli
    hlt
 
 
+; 00 is always black and 11 is always white
+draw_sprite:
+
+    push bp                ; save old base pointer
+    mov bp, sp             ; use the current stack pointer as new base pointer
+    pusha
+
+    mov cx, [bp + 12]       ; x coordinate
+    mov dx, [bp + 14]       ; y coordinate
+    
+                            ; initializing to 0, saves one byte from using mov
+    xor si, si              ; index of the bit we are checking (width)
+    xor di, di
+
+.row: ; main loop, this will iterate over every bit of [rock], if it is a 1 the .one part will be executed, if it is a 0 the .zero part will
+  
+    cmp si, 16      ; check if we have to move to the next byte/row
+    jne .same_row  ; Byte checked
+
+    xor si, si     ; this executes if we move to the next row
+    cmp di, 32     ; if we have finished with the tile
+    je .done
+    add di, 2      ; next row
+    inc dx
+
+    mov cx, [bp + 12]       ; x coordinate
+
+.same_row:
+
+    xor bh, bh              ; store the color
+
+    mov ax, [wiseman + di]
+    bt ax, si              ; first bit
+    jnc .next_bit
+    add bh, 1
+
+.next_bit:
+    inc si
+    bt ax, si              ; second bit
+    jnc .end_bit
+    add bh, 2
+
+.end_bit:
+    cmp bh, 0              ; black
+    je .pass
+
+    cmp bh, 1              ; first_color
+    je .first_color
+
+    cmp bh, 2              ; second_color
+    je .second_color
+
+    cmp bh, 3              ; white
+    je .white
+
+.first_color:
+
+    ; draw
+    mov ah, 0Ch
+    xor bh, bh             ; page number 0
+    mov al, 0Dh            ; Brown
+    int 10h
+    jmp .pass
+
+.second_color:
+
+    ; draw
+    mov ah, 0Ch
+    xor bh, bh             ; page number 0
+    mov al, 0Eh            ; Brown
+    int 10h
+    jmp .pass
+
+.white:
+
+    ; draw
+    mov ah, 0Ch
+    xor bh, bh             ; page number 0
+    mov al, 0Fh            ; Brown
+    int 10h
+    jmp .pass
+
+.pass:
+    inc si
+    inc cx
+    jmp .row
+
+.done:
+    popa
+    mov sp, bp
+    pop bp
+    ret 4
+    
+
 fire:    dw 0x2020, 0x8020, 0x8800, 0xA810, 0xA880, 0xA288, 0xA6A8, 0xAAA2, 0x9AA2, 0x66AA, 0x55AA, 0x7568, 0xFD68, 0xF5A0, 0x56A0, 0xAA00 ; 32 bytes
+;fire:    dw 0xFFFF, 0x8020, 0x8800, 0xA810, 0xA880, 0xA288, 0xA6A8, 0xAAA2, 0x9AA2, 0x66AA, 0x55AA, 0x7568, 0xFD68, 0xF5A0, 0x56A0, 0xFFFF ; 32 bytes
 wiseman: dw 0x5400, 0x7700, 0x4500, 0x4500, 0x5E00, 0xFF80, 0x0FA0, 0xFBE8, 0xFAE9, 0xFAA9, 0xE8A9, 0xA8A8, 0xA8A8, 0xAA20, 0xAA00, 0x9680 ; 32 bytes
 gef:     dw 0xAA00, 0xAA80, 0xAAA0, 0x88A8, 0x1818, 0x5158, 0x5558, 0x56A8, 0x5558, 0x5018, 0x5ED8, 0x5ED0, 0x9550, 0x65A0, 0x2A80, 0xBC00, 0x3F00, 0x33C0, 0x30F0, 0x3040, 0x0000, 0x2000; 44 bytes
 
